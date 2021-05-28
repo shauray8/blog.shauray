e4c3693bbfa73fa194e1a45a62aed0734395f7e5
Shauray <shauray9@gmail.com>
2021-05-28 16:44:01 +0530

    still brushing up my django skills and with that writing the blog as well

--- WeMustKnow/api/models.py
+++ WeMustKnow/api/models.py
@@ -12,8 +12,8 @@ def generate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
-    host = models.CharField(max_length=50, unique=True)
+    code = models.CharField(max_length=200, default="", unique=True)
+    host = models.CharField(max_length=50, unique=False)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    votes = models.IntegerField(null=False, default=1)
+    votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
