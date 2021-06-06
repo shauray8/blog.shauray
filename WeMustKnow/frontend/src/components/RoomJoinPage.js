e7b314139a9c735427aa2f24c9a2ae6d48d45a88
Shauray <shauray9@gmail.com>
2021-06-06 14:53:52 +0530

    getting into HTML

--- WeMustKnow/frontend/src/components/RoomJoinPage.js
+++ WeMustKnow/frontend/src/components/RoomJoinPage.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
+import button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Grid";
+import TextFiled from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
 
 export default class RoomJoinPage extends Component {
+    defaultVotes = 2;
+    
     constructor(props) {
         super(props);
     }
 
     render(){
 
-        return <p>Pharmaceuticals</p>
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="Center"></Grid>
+        </Grid>;
     }
 }
