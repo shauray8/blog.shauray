5bdb1b1faac6c241171b97c08cd38312b70cf054
Shauray <shauray9@gmail.com>
2021-07-14 13:20:47 +0530

    done with the main page

--- App.js
+++ App.js
@@ -24,12 +24,14 @@ function init() {
     const contact = document.querySelector(".contact")
     const social = document.querySelector(".social")
     const logo = document.querySelector('#logo')
+    const about = document.querySelector('.About')
 
     const Hl = new TimelineMax({ paused: true, reversed: true })
 
     Hl.to(navOpen, 0.5, { y: 0 })
         .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, '-=.1')
         .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, '-=0.5')
+        .fromTo(about, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, '-=0.5')
         .fromTo(logo, 0.2, { color: "white" }, { color: "#333a33" }, '-=1')
         .fromTo(hamburgerLines, 0.2, { stroke: "white" }, { stroke: "#333a33" }, "-=.5")
 
